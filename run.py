daffacf063ce07e2f777a1e6fb232db5ac389a53
Emily Ling <eling8@stanford.edu>
2018-01-06 15:28:06 -0800

    Basic model running

--- run.py
+++ run.py
@@ -5,13 +5,21 @@ import os
 import random
 import shutil
 
+from keras import optimizers
+from keras.models import Sequential, Model
+from keras.layers import Dropout, Flatten, Dense, Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+
 TEST_IMAGE = "pac_data/02/0/20160930_082811_796.npz"
 
 OLD_DATA_PATH = "pac_data"
 DATA_PATH = "data"
 TRAIN_PATH = os.path.join(DATA_PATH, "train")
 VALIDATION_PATH = os.path.join(DATA_PATH, "validation")
+IMG_WIDTH = 320
+IMG_HEIGHT = 240
 
+LEARNING_RATE = 0.0005
 VALIDATION_PERCENTAGE = 0.3
 RANDOM_SEED = 0
 
@@ -66,6 +74,14 @@ def split_and_rearrange_data():
                 shutil.copy(os.path.join(label_dir, all_files[i]),
                             os.path.join(TRAIN_PATH, label))
 
+"""
+Shuffles 2 numpy arrays of the same length together
+"""
+def shuffle_in_unison(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
+
 def load_depth_map(filename):
     return np.load(filename)['x'].astype(np.float32)
 
@@ -80,9 +96,99 @@ def depth_map_to_image(depth_map):
     img = cv2.applyColorMap(img, cv2.COLORMAP_OCEAN)
     return img
 
-split_and_rearrange_data()
-depth_map = load_depth_map(TEST_IMAGE)
-print(depth_map)
-cv2.imshow("Image", depth_map_to_image(depth_map))
-cv2.waitKey(0)
+def display_image(depth_map):
+    cv2.imshow("Image", depth_map_to_image(depth_map))
+    cv2.waitKey(0)
 
+def add_dimension(arr):
+    return np.expand_dims(arr, axis=3)
+
+"""
+Create a small dataset for testing purposes
+Returns data + labels for both train and validation sets
+Takes only a percentage (10%) of the first sensor's data
+"""
+def create_small_data():
+    print("Creating small dataset...")
+    PERCENTAGE = 0.10
+    train_data, train_labels = np.array([]), np.array([])
+    valid_data, valid_labels = np.array([]), np.array([])
+
+    for sensor_id in os.listdir(OLD_DATA_PATH):
+        sensor_dir = os.path.join(OLD_DATA_PATH, sensor_id)
+
+        for label in os.listdir(sensor_dir):
+            label_dir = os.path.join(sensor_dir, label)
+            label = int(label)
+
+            # Shuffle files within specific sensor/label
+            all_files = os.listdir(label_dir)
+            random.shuffle(all_files)
+            num_to_use = int(math.floor(len(all_files) * PERCENTAGE))
+            all_files = all_files[:num_to_use]
+
+            # Select first VALIDATION_PERCENTAGE as validation set
+            validation_num = int(math.floor(
+                VALIDATION_PERCENTAGE * len(all_files)))
+            valid_set = [load_depth_map(os.path.join(label_dir, all_files[i]))
+                         for i in range(0, validation_num)]
+            train_set = [load_depth_map(os.path.join(label_dir, all_files[i]))
+                         for i in range(validation_num, len(all_files))]
+
+            # Add data
+            if len(train_data) == 0:
+                train_data = train_set
+                valid_data = valid_set
+            else:  
+                valid_data = np.append(valid_data, valid_set, axis=0)
+                train_data = np.append(train_data, train_set, axis=0)
+
+            valid_labels = np.append(valid_labels, [label] * len(valid_set))
+            train_labels = np.append(train_labels, [label] * len(train_set))
+
+        break  # Use just one sensor for small data
+
+    train_data, train_labels = shuffle_in_unison(train_data, train_labels)
+    train_data = add_dimension(train_data)
+    valid_data, valid_labels = shuffle_in_unison(valid_data, valid_labels)
+    valid_data = add_dimension(valid_data)
+
+    return train_data, train_labels, valid_data, valid_labels
+
+def create_model():
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), padding="same", input_shape=(IMG_HEIGHT, IMG_WIDTH, 1), data_format='channels_first'))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (2, 2), padding="same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
+
+    model.add(Flatten())
+    model.add(Dense(256))
+    model.add(Activation("relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer=optimizers.Adam(lr=LEARNING_RATE),
+                  metrics=['accuracy'])
+
+    return model
+
+# https://stackoverflow.com/questions/43547402/how-to-calculate-f1-macro-in-keras
+
+split_and_rearrange_data()
+train_data, train_labels, valid_data, valid_labels = create_small_data()
+print("Training data:")
+print(train_data.shape, train_labels.shape)
+
+model = create_model()
+model.fit(x=train_data, 
+          y=train_labels, 
+          batch_size=20, 
+          epochs=10,
+          verbose=2,
+          validation_data=(valid_data, valid_labels))
